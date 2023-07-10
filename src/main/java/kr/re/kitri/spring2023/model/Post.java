67b45b6d6768c14616ab092ec605df4e25734ad3
eunjin Lee <leeunzin@gmail.com>
2023-07-10 17:15:17 +0900

    initial commit

---
+++ src/main/java/kr/re/kitri/spring2023/model/Post.java
@@ -0,0 +1,44 @@
+package kr.re.kitri.spring2023.model;
+
+public class Post {
+
+    private int id;
+    private String title;
+    private String body;
+    private String author;
+
+    public Post() {}
+
+    public Post(int id, String title, String body, String author) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+        this.author = author;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", body='" + body + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+}
